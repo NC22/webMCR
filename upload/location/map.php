95190c6152d28484937f58836f09460a884613e6
Kerr <svetlois@yandex.ru>
2013-04-26 00:35:39 +0400

    Add new menu items

---
+++ upload/location/map.php
@@ -0,0 +1,4 @@
+<?php if (!defined('MCR')) exit;
+$page = 'Карта'; $content_main = Menager::ShowStaticPage(STYLE_URL.'map.html');
+$menu->SetItemActive($menu_items['map']);
+?>
---
+++ upload/location/rules.php
@@ -0,0 +1,4 @@
+<?php if (!defined('MCR')) exit;
+$page = 'Правила сервера'; $content_main = Menager::ShowStaticPage(STYLE_URL.'rules.html');
+$menu->SetItemActive($menu_items['rules']);
+?>
