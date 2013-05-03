0eb8b405de813adf4ab81a57cce8f4a8f0cfb738
NC22 <torrenttvi@gmail.com>
2013-05-04 10:00:40 +1100

    correct syntax error 2

--- upload/instruments/auth/xauth.php
+++ upload/instruments/auth/xauth.php
@@ -33,7 +33,7 @@ public static function createPass($password) {
 
 	$salt = substr(hash('whirlpool', uniqid(rand(), true)), 0, 12);	
 	$hash = hash('whirlpool', $salt . $password);
-	$saltPos = (strlen($password) >= strlen($hash) ? strlen($hash) : strlen($password));
+	$saltPos = (strlen($password) >= strlen($hash)) ? strlen($hash) : strlen($password);
 	
 return substr($hash, 0, $saltPos) . $salt . substr($hash, $saltPos);     
 }
